f25283325d76de63f7b241772c88e4d799a90b0a
Quan Truong <truonghoangquan456@gmail.com>
2025-03-02 18:16:15 -0500

    Finish the sketch pad project
    Everything finish, now user can draw some things on it.
    It support click to draw and change the number of squares per dimension

--- script.js
+++ script.js
@@ -13,7 +13,39 @@ function createGrid(size, grid_size) {
         }
         grid.append(row); 
     }
-
 }
 
-createGrid(16, "600px");
+let tracking = false;
+document.addEventListener("mousedown", (e)=>{
+    tracking = true;
+    if (e.target.className == "square") {
+        e.target.style.backgroundColor = "black";
+    }
+});
+
+document.addEventListener("mouseup", ()=>{
+    tracking = false;
+});
+
+document.addEventListener("mousemove", (e)=>{
+    if (tracking && e.target.className == "square") {
+        e.target.style.backgroundColor = "black";
+    }
+});
+
+let size = 16;
+createGrid(size, "650px");
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", ()=> {
+    let num = prompt("How many squares per side do you want?");
+    grid.innerHTML = "";
+    size = num;
+    createGrid(num, "650px");
+});
+
+let reset = document.getElementById("reset");
+reset.addEventListener("click", () =>{
+    grid.innerHTML = "";
+    createGrid(size, "650px");
+});
